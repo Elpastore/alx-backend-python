cb8f96ca0df2784957413268563264823a83f254
elpastore <abdoulayesadio1997@gmail.com>
2024-05-02 15:47:29 +0000

    task7_v2

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """
 module 7-to_kv
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     returns a tuple
     """
