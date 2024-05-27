a6565f7205fa2a1a456bb904b61702029b955740
elpastore <abdoulayesadio1997@gmail.com>
2024-05-27 13:55:33 +0000

    task6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -48,6 +48,60 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(result, expected_url)
 
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test that public_repos returns the correct list of repos"""
+        test_payload = [
+            {
+                "name": "yimbo",
+                "id": 2233424,
+                "private": False,
+                "owner": {
+                    "login": "google",
+                    "id": 240297
+                    },
+                "fork": False,
+                "url": "https://api.github.com/orgs/google/yimbo",
+                "created_at": "2024-03-12T00:00:12Z",
+                "updated_at": "2024-04-23T00:43:52Z",
+                "has_issues": False,
+                "forks": 46,
+                "default_branch": "main"
+            },
+            {
+                "name": "genealatics",
+                "id": 3433452,
+                "private": False,
+                "owner": {
+                    "login": "google",
+                    "id": 240297
+                    },
+                "fork": False,
+                "url": "https://api.github.com/orgs/google/genealatics",
+                "created_at": "2024-04-07T00:40:12Z",
+                "updated_at": "2024-05-13T00:53:52Z",
+                "has_issues": False,
+                "forks": 156,
+                "default_branch": "main"
+            }
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            url = "https://api.github.com/orgs/test_org/repos"
+            mock_public_repos_url.return_value = url
+
+            client = GithubOrgClient("test_org")
+            result = client.public_repos()
+
+            # Check if the result matches the expected list of repo names
+            self.assertEqual(result, ["yimbo", "genealatics"])
+
+            # Check if the mocked property and get_json were called once
+            mock_public_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(url)
+
 
 if __name__ == "__main__":
     unittest.main()
