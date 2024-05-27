3450992e94284ded1bb83a49b2656bbfd7e98d78
elpastore <abdoulayesadio1997@gmail.com>
2024-05-27 12:03:30 +0000

    task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 test_utils module
 """
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from unittest.mock import Mock, patch
 import requests
@@ -54,6 +54,35 @@ class TestGetJson(unittest.TestCase):
         mocked_get.assert_called_once_with(test_url)
 
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    TestMemoize class
+    """
+    def test_memoize(self):
+        """
+        test memoize
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_memoize = TestClass()
+
+        with patch.object(test_memoize, 'a_method',
+                          return_value=42) as mocked_method:
+            result1 = test_memoize.a_property
+            result2 = test_memoize.a_property
+
+            mocked_method.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result1, 42)
 
 
 if __name__ == "__main__":
